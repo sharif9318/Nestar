14a1679f07b2eadda6b3dc50adc028c59941b8a4
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-09-27 23:43:30 +0900

    feat: hashing password via bycriptjs

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -5,13 +5,16 @@ import { Member } from '../../libs/dto/member/member';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { MemberStatus } from '../../libs/enums/member.enum';
 import { Message } from '../../libs/enums/common.enum';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class MemberService {
-    constructor(@InjectModel('Member') private readonly memberModel: Model<Member>) {}
+    constructor(@InjectModel('Member') private readonly memberModel: Model<Member>, 
+    private authService: AuthService,
+  ) {}
 
 public async signup(input: MemberInput): Promise<Member> {
-    // 0000 Hash password
+    input.memberPassword = await this.authService.hashPassword(input.memberPassword);
     try {
     const result = await this.memberModel.create(input);
     // 0001 Authentication via TOKEN
@@ -36,7 +39,7 @@ public async login(input: LoginInput): Promise<Member> {
     }
 
     // TODO: Compare passwords
-    const isMatch = memberPassword === response.memberPassword;
+    const isMatch = await this.authService.comparePasswords(memberPassword, response.memberPassword);
     if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
 
     return response;
