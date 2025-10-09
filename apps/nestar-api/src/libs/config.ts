42ce2d8f34c8b5d2381439c91d3164ebd5d152be
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-10-09 16:41:40 +0900

    feat: integrate comment module related DTOs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -2,7 +2,6 @@ import { ObjectId } from 'bson';
 
 export const availableAgentSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews', 'memberRank'];
 export const availableMemberSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews'];
-
 export const availableOptions = ['propertyBarter', 'propertyRent'];
 export const availablePropertySorts = [
   'createdAt',
@@ -14,7 +13,7 @@ export const availablePropertySorts = [
 ];
 
 export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews'];
-
+export const availableCommentSorts = ['createdAt', 'updatedAt'];
  // IMAGE CONFIGURATION 
 import { v4 as uuidv4 } from 'uuid';
 import * as path from 'path';
