2a4d4fbf5c618640dd26133f8dc3781c1fcb3a1c
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-10-15 12:36:27 +0900

    feat: develop subscribe, unsubscribe, getMemberFollowings and getMemberFollowers GraphQL APIs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -4,17 +4,17 @@ export const availableAgentSorts = ['createdAt', 'updatedAt', 'memberLikes', 'me
 export const availableMemberSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews'];
 export const availableOptions = ['propertyBarter', 'propertyRent'];
 export const availablePropertySorts = [
-  'createdAt',
-  'updatedAt',
-  'propertyLikes',
-  'propertyViews',
-  'propertyRank',
-  'propertyPrice',
+	'createdAt',
+	'updatedAt',
+	'propertyLikes',
+	'propertyViews',
+	'propertyRank',
+	'propertyPrice',
 ];
 
 export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews'];
 export const availableCommentSorts = ['createdAt', 'updatedAt'];
- // IMAGE CONFIGURATION 
+// IMAGE CONFIGURATION
 import { v4 as uuidv4 } from 'uuid';
 import * as path from 'path';
 
@@ -25,14 +25,32 @@ export const getSerialForImage = (filename: string) => {
 };
 
 export const shapeIntoMongoObjectId = (target: any) => {
-    return typeof target === 'string' ? new ObjectId(target) : target;
+	return typeof target === 'string' ? new ObjectId(target) : target;
 };
 
 export const lookupMember = {
-    $lookup: {
-    from: 'members',
-    localField: 'memberId',
-    foreignField: '_id',
-    as: 'memberData',
-    },
+	$lookup: {
+		from: 'members',
+		localField: 'memberId',
+		foreignField: '_id',
+		as: 'memberData',
+	},
+};
+
+export const lookupFollowingData = {
+	$lookup: {
+		from: 'members',
+		localField: 'followingId',
+		foreignField: '_id',
+		as: 'followingData',
+	},
+};
+
+export const lookupFollowerData = {
+	$lookup: {
+		from: 'members',
+		localField: 'followerId',
+		foreignField: '_id',
+		as: 'followerData',
+	},
 };
